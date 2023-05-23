d7c7bac725831d110003db534dc4360376ff38d6
AntonBuch <anton.buch@bayer.com>
2023-05-23 08:56:01 +0200

    added functionality to weibull

--- main.py
+++ main.py
@@ -23,11 +23,17 @@ st.header("Weibull Verteilung")
 c = st.slider('Formparameter', min_value=0.1, max_value=5.0, value=1.0, step=0.1)
 scale= st.slider('Lageparameter', min_value=0.1, max_value=5.0, value=1.0, step=0.1)
 loc = st.slider('Schwellenparameter', min_value=0.1, max_value=5.0, value=0.0, step=0.1)
+line = st.checkbox('Gerade bei 63,2%')
 
 fig_wei, ax_wei = plt.subplots(1, 1)
 x_wei = np.linspace(0,5, 100)
-ax_wei.plot(x_wei, weibull_min.pdf(x_wei, c, loc= loc, scale= scale))
+ax_wei.plot(x_wei, weibull_min.pdf(x_wei, c, loc= loc, scale= scale), label="Dichtefunktion")
+ax_wei.plot(x_wei, weibull_min.cdf(x_wei, c, loc= loc, scale= scale), label="Verteilungsfunktion")
+if line:
+    ax_wei.plot([0,scale+loc],[0.632,0.632], linestyle='dotted', color='black')
+    ax_wei.vlines([scale+loc],0,weibull_min.cdf(scale+loc, c, loc= loc, scale= scale), linestyles='dotted', colors='black')
 
+ax_wei.legend(loc='best', frameon=False)
 
 st.pyplot(fig_wei)
 
